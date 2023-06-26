e39add16c03a1b1b34ed418a1e75c9d295dc8fb2
Amritpal Singh <amritpalsingh@cssoftsolutions.com>
2023-06-26 19:52:21 +0530

    used hangfire in this

--- Console_App/Program.cs
+++ Console_App/Program.cs
@@ -1,18 +1,56 @@
 ﻿using Azure.Messaging.ServiceBus;
-using Microsoft.Azure.Amqp.Framing;
+using Hangfire;
+using Hangfire.MemoryStorage;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Threading.Tasks;
-
+using System.IO;
+using System.Threading;
 
 namespace linkedin_App
 {
     class Program
     {
+     static bool isFirstRequest = true;
+
         static async Task Main(string[] args)
+        {
+            ConfigureHangfire();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void ConfigureHangfire()
+        {
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started.");
+
+                if (isFirstRequest)
+                {
+                    // Run immediately for the first request
+                    ScrapeData();
+                    isFirstRequest = false;
+                }
+
+                RecurringJob.AddOrUpdate(() => ScrapeData(), Cron.MinuteInterval(5));
+
+                Console.WriteLine("Scraping job scheduled to run every 5 minutes.");
+
+                while (true)
+                {
+                    // Sleep for a short duration before checking for new jobs again
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public async static Task  ScrapeData()        
         {
             // Load appsettings.json configuration
             var configuration = new ConfigurationBuilder()
@@ -21,20 +59,28 @@ namespace linkedin_App
                 .Build();
 
             string[] proxyIPAddresses = configuration.GetSection("ProxyIPAddresses").Get<string[]>();
+            string username = configuration.GetValue<string>("Username")!;
+            string password = configuration.GetValue<string>("Password")!;
             string connectionString = configuration.GetConnectionString("ServiceBusConnection")!;
             string receiveQueueName = configuration.GetValue<string>("ReceiveQueueName")!;
             string sendQueueName = configuration.GetValue<string>("SendQueueName")!;
-            string username = configuration.GetValue<string>("Username")!;
-            string password = configuration.GetValue<string>("Password")!;
 
-            ServiceBusClient client = new ServiceBusClient(connectionString);
-            ServiceBusReceiver receiver = client.CreateReceiver(sendQueueName);
+            var proxy = new Proxy();
+            proxy.IsAutoDetect = false;
+            proxy.HttpProxy = proxyIPAddresses[new Random().Next(0, proxyIPAddresses.Length)];
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.Proxy = proxy;
+            // options.AddArgument("--proxy-server=" + proxy);
 
-            ServiceBusReceivedMessage[] receivedMessages;
-
-            while (true)
+            using (IWebDriver driver = new ChromeDriver(options))
             {
-                receivedMessages = (await receiver.ReceiveMessagesAsync(maxMessages: 100)).ToArray();
+                Login(driver, username, password);
+
+                ServiceBusClient client = new ServiceBusClient(connectionString);
+                ServiceBusReceiver receiver = client.CreateReceiver(sendQueueName);
+                ServiceBusReceivedMessage[] receivedMessages;
+                receivedMessages = receivedMessages = (await receiver.ReceiveMessagesAsync(maxMessages: 100)).ToArray();
 
                 if (receivedMessages.Any())
                 {
@@ -42,63 +88,40 @@ namespace linkedin_App
                     {
                         string linkedInProfileLink = message.Body.ToString();
                         string partitionKey = message.PartitionKey;
-                        var proxy = new Proxy();
-                        proxy.IsAutoDetect = false;
-                        proxy.HttpProxy = proxyIPAddresses[new Random().Next(0, proxyIPAddresses.Length)];
-                        ChromeOptions options = new ChromeOptions();
-                        options.AddArgument("--headless");
-                        options.Proxy = proxy;
-                        //options.AddArgument("--proxy-server=" + proxy);
-                        using (IWebDriver driver = new ChromeDriver(options))
-                        {
-                            Login(driver, username, password);
-                            var profileData1 = await ProcessUser(driver, linkedInProfileLink);
-                            var profileData = JsonConvert.DeserializeObject<ProfileData>(profileData1);
+                        var profileData = ProcessUser(driver, linkedInProfileLink);
 
-                            Console.WriteLine("Response:");
-                            Console.WriteLine(JsonConvert.SerializeObject(profileData, Formatting.Indented));
+                        Console.WriteLine("Scraped data:");
+                        Console.WriteLine(JsonConvert.SerializeObject(profileData, Formatting.Indented));
 
-                            ServiceBusSender sender = client.CreateSender(receiveQueueName);
-                            ServiceBusMessage responseMessage = new ServiceBusMessage(profileData1);
-                            responseMessage.PartitionKey = partitionKey;
-                            await sender.SendMessageAsync(responseMessage);
-                        }
+                        ServiceBusSender sender = client.CreateSender(receiveQueueName);
+                        ServiceBusMessage responseMessage = new ServiceBusMessage(JsonConvert.SerializeObject(profileData));
+                        responseMessage.PartitionKey = partitionKey;
+                        sender.SendMessageAsync(responseMessage).GetAwaiter().GetResult();
                     }
-                }
-                else
-                {
-                    // Wait for a short duration before checking for new messages again
-                    await Task.Delay(1000);
                 }
             }
         }
-
-
+        
         static void Login(IWebDriver driver, string username, string password)
         {
-
             driver.Navigate().GoToUrl("https://www.linkedin.com/login");
             driver.FindElement(By.Id("username")).SendKeys(username);
             driver.FindElement(By.Id("password")).SendKeys(password);
             driver.FindElement(By.XPath("//button[@type='submit']")).Click();
         }
 
-        static async Task<string> ProcessUser(IWebDriver driver, string linkedInProfileLink)
+        static ProfileData ProcessUser(IWebDriver driver, string linkedInProfileLink)
         {
             driver.Navigate().GoToUrl(linkedInProfileLink);
-            await Task.Delay(2000);
+            // Add necessary delay or wait conditions for page loading and data extraction
+
             ProfileData profileData = new ProfileData();
             profileData.ProfilePicUrl = FindProfilePictureUrl(driver);
             profileData.BackgroundCoverImageUrl = FindBackgroundCoverImageUrl(driver);
             profileData.FullName = FindElementText(driver, By.CssSelector(".text-heading-xlarge.inline.t-24.v-align-middle.break-words"));
             profileData.Headline = FindElementText(driver, By.CssSelector(".text-body-medium.break-words"));
 
-            Console.WriteLine("Profile Picture URL: " + profileData.ProfilePicUrl);
-            Console.WriteLine("Background Cover Image URL: " + profileData.BackgroundCoverImageUrl);
-            Console.WriteLine("Full Name: " + profileData.FullName);
-            Console.WriteLine("Headline: " + profileData.Headline);
-            return JsonConvert.SerializeObject(profileData);
-
+            return profileData;
         }
 
         static string FindProfilePictureUrl(IWebDriver driver)
@@ -111,7 +134,7 @@ namespace linkedin_App
             catch (NoSuchElementException)
             {
                 Console.WriteLine("Profile picture not found.");
-                return null!;
+                return null;
             }
         }
 
@@ -125,7 +148,7 @@ namespace linkedin_App
             catch (NoSuchElementException)
             {
                 Console.WriteLine("Background cover image not found.");
-                return null!;
+                return null;
             }
         }
 
@@ -139,16 +162,16 @@ namespace linkedin_App
             catch (NoSuchElementException)
             {
                 Console.WriteLine("Element not found: " + locator.ToString());
-                return null!;
+                return null;
             }
         }
     }
 
     class ProfileData
     {
-        public string ProfilePicUrl { get; set; } = string.Empty;
-        public string BackgroundCoverImageUrl { get; set; } = string.Empty;
-        public string FullName { get; set; } = string.Empty;
-        public string Headline { get; set; } = string.Empty;
+        public string ProfilePicUrl { get; set; }
+        public string BackgroundCoverImageUrl { get; set; }
+        public string FullName { get; set; }
+        public string Headline { get; set; }
     }
 }
